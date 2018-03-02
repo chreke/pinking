7a26c2c69bd4c8f071f65bacabe4f985a2088820
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2018-03-02 16:01:56 +0100

    Update PinViewSet

--- src/pin/views.py
+++ src/pin/views.py
@@ -1,8 +1,23 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from .serializers import PinSerializer
 from .models import Pin
+from .ipfs import MockIPFS
 
 
-class PinViewSet(viewsets.ModelViewSet):
+class PinViewSet(
+        mixins.CreateModelMixin,
+        mixins.DestroyModelMixin,
+        mixins.ListModelMixin,
+        mixins.RetrieveModelMixin,
+        viewsets.GenericViewSet
+):
     queryset = Pin.objects.all()
     serializer_class = PinSerializer
+
+    def perform_create(self, serializer):
+        ipfs = MockIPFS('0.0.0.0')
+        multihash = serializer.validated_data['multihash']
+        serializer.save(
+            user=self.request.user,
+            cumulative_size=ipfs.get_cumulative_size(multihash),
+        )
