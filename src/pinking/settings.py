4a2551500bed65f7063091ee2c66e6170d73de84
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2018-03-02 15:03:05 +0100

    Set up DRF

--- src/pinking/settings.py
+++ src/pinking/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'pin',
 ]
 
@@ -123,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
