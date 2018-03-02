b85402e0140e09dad78b71e8b774b9ec35799938
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2018-03-02 15:04:38 +0100

    Add Pin model

--- src/pin/models.py
+++ src/pin/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Pin(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    cumulative_size = models.PositiveIntegerField(
+        help_text='Total size of object and its references in bytes'
+    )
+    multihash = models.CharField(max_length=64, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
