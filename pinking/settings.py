9c100561fb2bb4a37eb1d72c5293a50e3e8a5b59
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2018-03-13 07:41:14 +0100

    Only allow authenticated users to access API

--- pinking/settings.py
+++ pinking/settings.py
@@ -127,7 +127,11 @@ STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated'
     ]
 }
