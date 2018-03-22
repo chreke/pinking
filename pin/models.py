54582d92523b12cdbb9c2c1783d2e48de24d365c
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2018-03-22 11:48:26 +0100

    Change Pin model

--- pin/models.py
+++ pin/models.py
@@ -3,10 +3,20 @@ from django.db import models
 
 
 class Pin(models.Model):
+    RECURSIVE = 1
+    INDIRECT = 2
+    MFS_ROOT = 3
+    PIN_TYPE_CHOICES = (
+        (RECURSIVE, 'Recursive'),
+        (INDIRECT, 'Indirect'),
+        (MFS_ROOT, 'MFS root'),
+    )
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
+    block_size = models.PositiveIntegerField()
+    pin_type = models.PositiveIntegerField(choices=PIN_TYPE_CHOICES)
     multihash = models.CharField(
         max_length=64,
         db_index=True,
