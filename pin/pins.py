7f37753e9c0b2ddcb02c39574d0314ec1b8d25ab
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2018-03-22 16:39:25 +0100

    Filter out pins with count <= 0

--- pin/pins.py
+++ pin/pins.py
@@ -1,5 +1,5 @@
 def space_used(user):
     block_sizes = {}
-    for p in user.pins.all().iterator():
+    for p in user.pins.filter(count__gt=0).iterator():
         block_sizes[p.multihash] = p.block_size
     return sum(block_sizes.values())
